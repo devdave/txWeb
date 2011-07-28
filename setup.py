a9e815b38b8a0ce05943d149809e9c8e28953881
devdave <devdave@ominian.net>
2011-07-27 23:29:17 -0600

    Added setup and obligatory gitignore directives

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name="txweb"
+      , version='0.01'
+      , author = "David W."
+      , author_email = "txwebpypi@ominian.net"
+      , url = "https://github.com/devdave/txWeb"
+      , packages=['txweb']
+)
