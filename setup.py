a4ee658bb64d9a989ab65d9274b0aa59da9e5da3
devdave <devdave@ominian.net>
2011-10-02 14:54:41 -0600

    Changed version to numeric+date system plus started adding and non-obvious dependancies 'decorator' for example

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 setup(name="txweb"
-      , version='0.5'
+      , version='0.5.2011.10.2'
       , author = "David W."
       , author_email = "txwebpypi@ominian.net"
       , url = "https://github.com/devdave/txWeb"
-      , packages=['txweb']
+      , packages=['txweb',"txweb.sugar","txweb.util"]
       , package_data={'txweb': ['LICENSE.txt', 'tests/test_data/a.txt', 'tests/test_data/c.txt', 'tests/test_data/subdir/b.txt'  ]}
       , license = "MIT License"
       , keywords = "twisted web alternative routing"
       , description = "An alternative routing system for use with twisted.web"
+      , requires = ["decorator"]
 )
