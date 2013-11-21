d2eca365c65a5bbb48ae76c942c2ffeb960b6a61
David W <devdave@ominian.net>
2013-11-21 04:38:17 -0700

    Holy shit bad name, consolidate is* tests for routing logic

---
+++ txweb/util/is_a.py
@@ -0,0 +1,9 @@
+
+#TODO terrible module name
+
+#The following lambda's are common checks used to validating routing logic down the line
+isExposed  = lambda entity : getattr(entity, "exposed", False)
+#TODO should this be an instance check?
+isResource = lambda entity : callable(getattr(entity, "render", None))
+#TODO safe to assume if it's a resource that it should be exposed
+isAction = lambda entity : ( isExposed(entity) and callable(entity) ) or ( isResource(entity) )
