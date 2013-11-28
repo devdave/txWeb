3bcbb32ec6da92930b7cb59fa0d392cb7802fae3
David W <devdave@ominian.net>
2013-11-28 06:29:22 -0700

    Relocate exposed and ActionResource to core.py

--- txweb/core.py
+++ txweb/core.py
@@ -1,5 +1,3 @@
-#Module level
-from util import OneTimeResource
 #TODO terrible module name
 from util.is_a import isAction
 from util.is_a import isExposed
@@ -8,10 +6,11 @@ from util.is_a import isResource
 #Twisted level
 from twisted.web import server
 from twisted.web import resource
-from twisted.web import util
+from twisted.internet import defer
+from twisted.web.server import NOT_DONE_YET
 
-from twisted.web.resource import ErrorPage
-from twisted.web.resource import ForbiddenResource
+#from twisted.web.resource import ErrorPage
+#from twisted.web.resource import ForbiddenResource
 from twisted.web.resource import NoResource
 
 
@@ -32,12 +31,47 @@ from collections import OrderedDict
 
 """
 
-class CBranch(object):
+
+
+def expose(target):
     """
-        To simplify traversing/descending an object graph, make all branches
-        required to be an child class of CBranch.
+        A simple decorate to add the .exposed attribute
     """
-    pass
+    target.exposed = True
+    return target
+
+
+class ActionResource(resource.Resource):
+
+    __slots__ = ['func', 'parent']
+    exposed = True
+
+    def __init__(self, func, parent = None, path = None):
+        self.func = func
+        self.parent = parent
+        self.path = path
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __eq__(self, other):
+        #TODO lookinto using decorator for this stuff!
+        return other == self.func
+
+    def redirectTo(self, request, *args, **kwargs):
+        #TODO -
+        request.redirect(self.path)
+        request.finish()
+        return NOT_DONE_YET
+
+    def render(self, request):
+        response = self.func(request)
+        #If the response is a Deferred, tell the stack to stop pre-emptive
+        # cleanup as the show's not over yet
+        if isinstance(response, defer.Deferred):
+            response = NOT_DONE_YET
+
+        return response
 
 
 class CSite(server.Site):
@@ -53,6 +87,15 @@ class CSite(server.Site):
         server.Site.__init__(self, resource, logPath, timeout)
         self.object_graph = self._compute_path(self.resource, "")
 
+    def getResourceFor(self, request): #pragma: no cover
+        """
+            Copied from twisted.web.server.Site to allow for
+            hijacking into txweb routing system.
+        """
+        request.site = self
+        request.sitepath = copy.copy(request.prepath)
+        return self.routeRequest(request)
+
     def _compute_path(self, parent, path):
 
 
@@ -60,7 +103,11 @@ class CSite(server.Site):
         print parent, path
 
         if hasattr(parent, "index"):
-            graph[re.compile("^%s/$" % path) ] = parent.index
+            if isExposed(parent.index):
+                parent.index = graph[re.compile("^%s/$" % path) ] = ActionResource(parent.index, path = "%s/" % path)
+            else:
+                graph[re.compile("^%s/$" % path) ] = parent.index
+
 
 
         for name in dir(parent):
@@ -72,7 +119,10 @@ class CSite(server.Site):
             if isAction(obj):
                 node_body = "%s/%s" % (path, name,)
                 if isExposed(obj):
-                    graph[re.compile("^%s$" % node_body)] = obj
+                    action = graph[re.compile("^%s$" % node_body)] = ActionResource(obj, path = node_body)
+                    setattr(parent, name, action)
+
+
                 elif isResource(obj):
                     graph[re.compile("^%s/?(.*)" % node_body)] = obj
 
@@ -95,176 +145,15 @@ class CSite(server.Site):
 
         if action is None:
             return NoResource()
-        else:
-            #Last step is to see what was found, if the target was a resource, we're done
-            #For now, assuming only ^url/2/resource(.*)$ regexs are used for resources!
-
-            if isResource(action):
-                url_groups = url_match.groups()
-                if url_groups:
-                    childPath = url_match.groups()[0].split("/")
-                    while childPath:
-                        action = action.getChild(childPath.pop(0), request)
-
-                return action
-
-            else:
-                return OneTimeResource(action)
-
-
-class Site(server.Site):
-    """
-        A monkey patch that short circuits the normal
-        resource resolution logic @ the getResourceFor entry point
-
-    """
-
-
-    def routeRequest(self, request):
-        """
-            Traverses from the root controller, down through its children and returns the first callable or Resource that
-            matches the provided request.path
-            If there is no match, it will return NoResource
-
-            :param request is a twisted.web.request instance
-
-
-            ex. Given a path like '/foo/test/x' it would match a OO graph of root.foo.test.x or the first callable
-            in that line.  so if root.root() has a method called test, then 'x' will be pushed into
-
-
-        """
-        action = None
-
-        #self.resource is the Root controller object provided to Site.__init__(resource)
-        root = parent = self.resource
-        """
-            defaultAction is a catch_all method that is set to intercept routing fallouts
-            below the current parent.
-            Example:
-
-            Given
-                a request path like "/foo/bar/something/something/
-
-            and a Graph like
-
-                class Bar:
-                    pass
-
-                class Foo:
-                    bar = Bar()
-
-                class Root:
-                    def __default__(self, request):
-                        return "Root default handler"
-
-                    foo = Foo()
-
-
-                Root.__default__ will end up being called
-
-                if Foo had a __default__ method, it would be called instead
-                if Bar had a __default__ method, then naturally it would be called
-
-                The default action mechanism is the only planned means of supporting out of order routing
-                like "/person/123/edit" where the desired action is to call Root.person.edit(<Request args={record_id:123} )
-        """
-        defaultAction = getattr(root, "__default__", None ) or NoResource()
-
-
-        #crucial check because /foo/bar and /foo/bar/ should be treated as different ( former suggest Foo.<bound method bar> while the second Foo.<object instance bar>)
-        endedWithSlash = request.path.endswith("/")
-        path = request.path.strip("/").split("/")
-
-
-
-
-        for i, element in enumerate(path):
-
-            #For the first run through the loop, parent == root, but subsequent iterations parent will equal the Nth child of root
-            parent = root
-            root = getattr(root, element, None)
-            request.prepath.append(element)
-
-            if root is None:
-                """
-                    if root has nulled out, check to see if the last element in the path was a controller
-                    AND if so check for an index attribute that is callable or resource
-                """
-                if endedWithSlash:
-                    if hasattr(parent, "index") and isAction(getattr(parent, "index")):
-                        action = getattr(parent, "index")
-                break
-
-
-            if hasattr(root, "__default__") and isAction(getattr(root, "__default__")):
-                #Check for a catchall default action
-                defaultAction = getattr(root, "__default__")
-
-
-            if element.startswith("_"):
-                #500 simplistic security check
-                #There is already the exposed check, but this is a second security check to prevent calls to Root.__dict__ or such
-                return ErrorPage(500, "Illegal characters", "URI segments cannot start with an underscore")
-                break #pragma: no cover
-
-            if isAction(root):
-                action = root
-                request.postpath = path[i+1:]
-                if isResource(root):
-                    """
-                        NOTE:
-                            For twisted.web a path like "/foo/bar/blah" translates to ["","foo","bar","blah"]
-                            while "/foo/bar/blah/" == ["","foo","bar","blah",""] which is how Str.split works.
-                            twisted.web's pathing logic depends on the prior result for Resource child delegation.
-                            txweb meanwhile, doesn't.
-                    """
-                    if len( path[i+1:] ) > 0:
-                        childPath = request.postpath[:]
-                        while childPath:
-                            sub = childPath.pop(0)
-                            action = action.getChild(sub, request)
-                    elif endedWithSlash:
-                        #TODO need more unit-tests to clarify why I did this
-                        #given foo = File("somePath/") and a path like "/foo/", expected twisted behavior is File to be given path of ""
-                        action = action.getChild("", request)
-
-                break
-
-
-
-
-
-
-
-        if action is None:
-            if endedWithSlash and hasattr(root, "index"):
-                action = getattr(root, "index")
-
-            #handles calls to child object attributes where they're at root.foo and url == "/foo" but should be "/foo/"
-            elif not endedWithSlash and element in dir(parent) and element[0] != "_" and isinstance(getattr(parent, element), object):
-                action = util.Redirect("%s/" % request.path)
-
-        if action is None:
-            action = defaultAction or NoResource()
 
         #Last step is to see what was found, if the target was a resource, we're done
+        #For now, assuming only ^url/2/resource(.*)$ regexs are used for resources!
+
         if isResource(action):
-            return action
-        #If action was a non-resource callable, bind the callable's parent to allow for pre/post filter logic in OneTimeResource
-        elif parent is not None and isinstance(parent, object):
-            return OneTimeResource(action, parent)
-        else:
-            #This isn't ideal but could be a result of Root having a __call__ method
-            #Coverage in WebMud & PyProxy SSR both show this is never reached - placed on TODO as an Exception
-            return OneTimeResource(action)
+            url_groups = url_match.groups()
+            if url_groups:
+                childPath = url_match.groups()[0].split("/")
+                while childPath:
+                    action = action.getChild(childPath.pop(0), request)
 
-
-
-    def getResourceFor(self, request): #pragma: no cover
-        """
-            TODO should I add a try...catch here?
-        """
-        request.site = self
-        request.sitepath = copy.copy(request.prepath)
-        return self.routeRequest(request)
+        return action
