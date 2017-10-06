031b1ce37dd8793c4976bf121a7d410481007a67
devdave <devdave@ominian.net>
2017-10-06 17:05:19 -0600

    WS cleanup, TODO check what kind of EOL Komodo is using

--- txweb/sugar/__init__.py
+++ txweb/sugar/__init__.py
@@ -1,9 +1,9 @@
 """
     The Sugar module contains some relatively questionable code ( even for me ).
-    
+
     SmartController - Is a metaclass type that at run-time class definition automatically renames
     all controllers with the prefix "action_" into exposed methods.
-    
-    
+
+
 """
-from smartcontroller import SmartController
+from .smartcontroller import SmartController
