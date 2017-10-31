fa000f84e9a47a76a7ca795235c456eff42935f9
devdave <devdave@ominian.net>
2017-10-31 03:23:00 -0600

    cleanup to use the nose2 copied Unit Test helper class

--- txweb/tests/test_prepostfilters.py
+++ txweb/tests/test_prepostfilters.py
@@ -5,12 +5,15 @@ from txweb.core import Site
 from txweb.util import expose
 from txweb.util import OneTimeResource
 from txweb.util.testing import TestRequest
+from txweb.tests.helper import helper
 
 from twisted.web.test.test_web import DummyRequest
 from twisted.web.resource import ErrorPage
 from twisted.web.static import File
 from twisted.web.server import NOT_DONE_YET
 from twisted.web.static import DirectoryLister
+
+
 
 relPath = lambda filename : abspath(join(dirname(__file__), "test_data" , filename))
 
@@ -33,10 +36,15 @@ def test_filtersAreCalled():
     site = Site(root)
     request = TestRequest([],"/anAction")
     action = site.routeRequest(request)
-    assert isinstance(action, OneTimeResource)
-    assert action.func == root.anAction
+    helper.assertIsInstance(action, OneTimeResource)
+    helper.assertEqual(action.func, root.anAction)
+    
     response = action.render(request)
-    assert response == "anActionWasCalled"
-    assert getattr(root, "preFilterWasCalled", False) == True
-    assert getattr(root, "postFilterWasCalled", False) == True
+    helper.assertEqual(response, "anActionWasCalled".encode())
+    
+    helper.assertTrue(root.preFilterWasCalled)
+    helper.assertTrue(root.postFilterWasCalled)
 
+if __name__ == '__main__':
+    import nose2
+    nose2.main()
