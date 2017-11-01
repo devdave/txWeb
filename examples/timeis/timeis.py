7516618411463d5e5f637737990a3c8bedbbb767
devdave <devdave@ominian.net>
2017-11-01 06:50:07 -0600

    How did I ever get a job
    Seriously txweb is a mess, the correct example app never got checked in
    so I am remaking it now

---
+++ examples/timeis/timeis.py
@@ -0,0 +1,31 @@
+
+import time
+import webbrowser
+
+from txweb import Site as Site
+from txweb import expose
+
+
+
+from twisted.web.static import File
+from twisted.internet import reactor, defer
+from twisted.web.server import NOT_DONE_YET
+
+
+
+class Root(object):
+
+    @expose
+    def index(self, request):
+
+        return f"It is currently {time.time()}"
+
+
+def run():
+    reactor.listenTCP(8080, Site(Root()))
+    reactor.callLater(3, webbrowser.open, "http://127.0.0.1:8080/" )
+    reactor.run()
+
+if __name__ == '__main__':
+    print("Starting")
+    run()
