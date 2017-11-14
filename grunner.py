d3f63882723ccd7fcf26821d794857c1a08507cc
devdave <devdave@ominian.net>
2017-11-14 15:55:47 -0700

    Find and replace to change from EVENT to MESSAGE

--- grunner.py
+++ grunner.py
@@ -32,14 +32,14 @@ def watch_files(entry_point, additional = None):
     new_manifest = lambda m:find_files(m, entry_point, additional)
     manifest = new_manifest({})
     
-    state = EVENT.CONT    
-    while state == EVENT.CONT:
+    state = MESSAGE.CONT
+    while state == MESSAGE.CONT:
         state = yield state, None
 
         for pyfile, mtime in manifest.items():
             if pyfile.stat().st_mtime != mtime:
-                state = yield EVENT.CHANGED, pyfile
-                if state != EVENT.CONT:
+                state = yield MESSAGE.CHANGED, pyfile
+                if state != MESSAGE.CONT:
                     return
 
                 manifest = new_manifest({})
@@ -50,29 +50,29 @@ def run_process(entry_point):
 
     chdir = entry_point.parent.resolve()
     target = entry_point.resolve()
-    status = EVENT.CONT
+    status = MESSAGE.CONT
 
     try:
         process = subprocess.Popen([sys.executable, str(entry_point.resolve())], cwd=chdir)
         print("Process started")
     except Exception as ex:
-        # yield EVENT.CRASHED
+        # yield MESSAGE.CRASHED
         raise
 
-    status = yield EVENT.CONT, None
+    status = yield MESSAGE.CONT, None
 
-    while status == EVENT.CONT:
+    while status == MESSAGE.CONT:
         try:
             return_code = process.wait(.5)
-            status = yield EVENT.GRACEFUL, return_code
+            status = yield MESSAGE.GRACEFUL, return_code
 
         except subprocess.TimeoutExpired:
-            status = yield EVENT.CONT, None
+            status = yield MESSAGE.CONT, None
         except subprocess.CalledProcessError as exc:
-            status = yield EVENT.ERROR, exc.returncode
+            status = yield MESSAGE.ERROR, exc.returncode
 
     process.terminate()
-        
+
 
 class RUNMODE(Names):
     NORMAL = NamedConstant()
@@ -81,39 +81,42 @@ class RUNMODE(Names):
 
 def safe_stop(coroutine):
     try:
-        coroutine.send(EVENT.STOP)
+        coroutine.send(MESSAGE.STOP)
     except StopIteration:
         return True
 
     return False
 
+def safe_start(coroutine, *args, **kwargs):
+    #somewhat misnomer, more conveniance for now
+    cogen = coroutine(*args, **kwargs)
+    cogen.send(None)
+    return cogen
+
 def main():
     entry_point = pathlib.Path(sys.argv[1])
     additional = pathlib.Path(sys.argv[2]) if len(sys.argv) > 2 else None
 
-    watcher = watch_files(entry_point, additional)
-    process = run_process(entry_point)
+    watcher = safe_start(watch_files, entry_point, additional)
+    process = safe_start(run_process, entry_point)
     
     
     try:
-        process.send(None)
-        watcher.send(None)
         while True:
             
-            watch_state, watch_detail = watcher.send(EVENT.CONT)
+            watch_state, watch_detail = watcher.send(MESSAGE.CONT)
 
-            if watch_state != EVENT.CONT:
+            if watch_state != MESSAGE.CONT:
                 print(f"Watch: {watch_state}:{watch_detail}")
 
-                assert watch_state == EVENT.CHANGED, f"Watcher state unexpected {watch_state}:{watch_detail}"
+                assert watch_state == MESSAGE.CHANGED, f"Watcher state unexpected {watch_state}:{watch_detail}"
                 safe_stop(process)
                 del process
-                process = run_process(entry_point)
-                process.send(None)
+                process = safe_start(run_process, entry_point)
                 
-            proc_state, proc_detail = process.send(EVENT.CONT)
+            proc_state, proc_detail = process.send(MESSAGE.CONT)
 
-            if proc_state != EVENT.CONT:
+            if proc_state != MESSAGE.CONT:
                 print(f"Process ended with {proc_detail}")
                 return
 
