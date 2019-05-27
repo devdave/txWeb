b84010d318ad846553047ce27ab4b93d40e84a80
devdave <devdave@ominian.net>
2019-05-27 10:36:01 -0600

    maxe NOT DONE YET common variable

--- txweb/__init__.py
+++ txweb/__init__.py
@@ -3,4 +3,6 @@ from .util import expose
 from .core import Site
 from .core import CSite
 
-__all__ = ['Site', 'expose', 'CSite']
+from twisted.web.server import NOT_DONE_YET
+
+__all__ = ['Site', 'expose', 'CSite', 'NOT_DONE_YET']
