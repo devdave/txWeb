fb0853293b736d1bed8c7892778e07205de68075
devdave <devdave@ominian.net>
2019-05-27 10:45:23 -0600

    PEP 8

--- txweb/web_views.py
+++ txweb/web_views.py
@@ -3,10 +3,11 @@ import inspect
 import re
 from collections import OrderedDict
 
+
 def process_route(route_str, func, double_slash_warn=True):
     segments = route_str.split("/")
     raw_regex = []
-    scope = None
+
     match_rules = {}
 
     trailing_slash = route_str.endswith("/")
@@ -17,7 +18,7 @@ def process_route(route_str, func, double_slash_warn=True):
 
     for segment in segments:
         if segment.startswith("<"):
-            name, type = segment[1:-1].split(":")
+            name, _ = segment[1:-1].split(":")
             match_rules[name] = type
             re_segment = f"(?P<{name}>.*)"
             raw_regex.append(re_segment)
@@ -30,7 +31,6 @@ def process_route(route_str, func, double_slash_warn=True):
         else:
             raw_regex.append(segment)
 
-
     raw_regex.insert(0, "^")
     raw_regex = "/".join(raw_regex)
 
@@ -39,37 +39,31 @@ def process_route(route_str, func, double_slash_warn=True):
 
     raw_regex += "$"
 
-
     return WebRoute(raw_regex, func, rules=match_rules)
-
 
 
 class WebRoute(object):
 
-
-    def __init__(self, raw_regex, func, rules={}):
+    def __init__(self, raw_regex, func, rules=None):
         self.raw_regex = raw_regex
         self.func = func
         self.regex = re.compile(self.raw_regex)
+        self.rules = rules
         self.signature = inspect.signature(func)
         self.is_generator = inspect.isgenerator(func)
-
 
     def matches(self, url):
         return self.regex.match(url) is not None
 
     def run(self, request):
-        matches = self.regex.match(url)
+        matches = self.regex.match(request.url)
 
         vargs = [request]
-        #currently unused as is
+        # currently unused as is
         kwargs = matches.groupdict()
         vargs += kwargs.values()
 
         return self.func(*vargs)
-
-
-
 
 
 if __name__ == "__main__":
