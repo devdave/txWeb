6ab51e3b7a5d9c121e57508a917e68687a8daede
devdave <devdave@ominian.net>
2019-05-28 13:54:27 -0600

    Simplify website/web_view

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,121 +2,19 @@ from txweb import NOT_DONE_YET
 from txweb import web_views
 
 
-def hanging_view(_):
-    yield NOT_DONE_YET
-    yield "Hello World"
+def test__website_add__works():
 
-
-def string_view(_, word: str):
-    return word
-
-
-def test__process_route__matches():
-
-    def mock(_, number):
-        return _, number
-
-    # todo break in two tests
-
-    test_no_trail_url = "/foo/bar/123"
-    test_no_trail_route_str = "/foo/bar/<number:int>"
-    actual_no_trail_route = web_views.process_route(test_no_trail_route_str, mock)
-    actual_no_trail_matches = actual_no_trail_route.matches(test_no_trail_url)
-
-    assert actual_no_trail_matches is True
-
-
-def test__process_route__matches_trailing_slash():
-
-    def mock(_, number):
-        return _, number
-
-    test_trail_url = "/foo/bar/123/"
-    test_trail_route_str = "/foo/bar/<number:int>/"
-    actual_trail_route = web_views.process_route(test_trail_route_str, mock)
-    actual_trail_matches = actual_trail_route.matches(test_trail_url)
-
-    assert actual_trail_route.raw_regex == f"^/foo/bar/(?P<number>.*)/$"
-    assert actual_trail_matches is True
-
-
-def test__process_route__calls_int_type_correctly():
-
-    def stub(request, argument1):
-        return argument1
-
-    class Request123:
-        path = "/foo/bar/123/"
-
-
-    test_number_url = "/foo/bar/123/"
-    test_route_str = "/foo/bar/<number:int>/"
-    actual_route = web_views.process_route(test_route_str, stub)
-
-    actual_result = actual_route.run(Request123())
-    assert actual_result == 123
-
-
-def test__process_route__calls_str_type_correctly():
-
-    def stub(request, argument1):
-        return argument1
-
-    class Request123:
-        path = "/foo/bar/word/"
-
-    test_route_str = "/foo/bar/<a_word:str>/"
-    actual_route = web_views.process_route(test_route_str, stub)
-
-    actual_result = actual_route.run(Request123())
-    assert actual_result == "word"
-
-
-def test__add_route():
-
-    class RequestStubString:
-        path = "/foo/bar/thing_string/"
-
-    class RequestStubNumber:
-        path = "/foo/bar/4567/"
-
-    @web_views.add("/foo/blah/<thing_str:str>/")
-    def stub(request, thing_str):
-        return thing_str
-
-    @web_views.add("/foo/bar/<thing_number:int>")
-    def stub2(request, thing_int):
-        return thing_int
-
-    action = web_views.site.getResourceFor(RequestStubNumber())
-
-    assert action.func == stub2
-
-
-def test__Site_getResourceFor__returns_NoResource_if_no_match():
-
-    class RequestBad:
-        path = "/no/match/url"
+    class StubRequest:
+        path = "/foo/bar/890"
 
     @web_views.add("/foo/bar/<number:int>")
-    def stub(request, number):
-        return number
+    def stub(request, a_number):
+        return a_number
 
-    action = web_views.site.routeRequest(RequestBad())
-
-    assert isinstance(action, web_views.NoResource)
-
-
-def test_Site_getResourceFor__returns_ActionResource_if_match():
-
-    class MatchingPath:
-        path = "/foo/bar/a_number_follows/123/"
-
-    @web_views.add("/foo/bar/a_number_follows/<number:int>/")
-    def stub(request, number):
-        return number
+    resource = web_views.website.getResourceFor(StubRequest())
+    assert isinstance(resource, web_views.resource.Resource)
+    assert isinstance(resource, web_views.ViewResource)
+    assert resource.func == stub
+    assert resource.render(StubRequest()) == 890
 
 
-    action = web_views.site.routeRequest(MatchingPath())
-    assert isinstance(action, web_views.resource.Resource)
-    assert action.render(MatchingPath()) == 123
--- txweb/web_views.py
+++ txweb/web_views.py
@@ -1,82 +1,79 @@
+#txweb imports
+
 #twisted imports
 from twisted.web import server
 from twisted.web.resource import NoResource
 from twisted.web import resource
 
-from functools import wraps
-import sys
-import inspect
+#stdlib
 import re
 from collections import OrderedDict
 
 
+class ViewResource(resource.Resource):
 
+    isLeaf = True # Disable dynamic resource mechanism in twisted.web
 
-def process_route(route_str, func, double_slash_warn=True):
-    segments = route_str.split("/")
-    raw_regex = []
-
-    match_rules = {}
-
-    trailing_slash = route_str.endswith("/")
-
-    if "//" in route_str:
-        if double_slash_warn is True:
-            print("Warning: there is a double slash (//) in route")
-
-    for segment in segments:
-        if segment.startswith("<"):
-            name, name_type = segment[1:-1].split(":")
-            match_rules[name] = name_type
-            re_segment = f"(?P<{name}>.*)"
-            raw_regex.append(re_segment)
-
-        elif ">" in segment:
-            raise ValueError("Missing < to match >")
-
-        elif segment == "":
-            pass
-        else:
-            raw_regex.append(segment)
-
-    raw_regex.insert(0, "^")
-    raw_regex = "/".join(raw_regex)
-
-    if trailing_slash is True:
-        raw_regex += "/"
-
-    raw_regex += "$"
-
-    return WebRoute(raw_regex, func, rules=match_rules)
-
-
-class WebRoute(resource.Resource):
-
-    def __init__(self, raw_regex, func, rules=None):
-        self.raw_regex = raw_regex
+    def __init__(self, routing_str, func, double_slash_warning=True):
+        self.routing_str = routing_str
         self.func = func
-        self.regex = re.compile(self.raw_regex)
-        self.rules = rules
-        self.signature = inspect.signature(func)
-        self.is_generator = inspect.isgenerator(func)
+        self.double_slash_warning = double_slash_warning
+        self.regex = None
+        self.route_rules = None
 
-    def matches(self, url):
-        return self.regex.match(url) is not None
+        self.process_route(self.routing_str)
+
+        resource.Resource.__init__(self)
+
+    def process_route(self, routing_str):
+        segments = routing_str.split("/")
+        raw_regex = []
+        match_rules = {}
+
+        trailing_slash = routing_str.endswith("/")
+
+        if "//" in routing_str:
+            if self.double_slash_warning is True:
+                print("Warning: there is a double slash (//) in route")
+
+        for segment in segments:
+            if segment.startswith("<"):
+                name, name_type = segment[1:-1].split(":")
+                match_rules[name] = name_type
+                re_segment = f"(?P<{name}>.*)"
+                raw_regex.append(re_segment)
+
+            elif ">" in segment:
+                raise ValueError("Missing < to match >")
+
+            elif segment == "":
+                pass
+            else:
+                raw_regex.append(segment)
+
+        raw_regex.insert(0, "^")
+        raw_regex = "/".join(raw_regex)
+
+        if trailing_slash is True:
+            raw_regex += "/"
+
+        raw_regex += "$"
+
+        self.regex = re.compile(raw_regex)
+        self.route_rules = match_rules
 
     def run(self, request):
         matches = self.regex.match(request.path)
 
-        vargs = [request]
+        vargs = [request, self]
         kwargs = OrderedDict()
-
 
         def eval_type(type_str, value):
             transform = eval(type_str, self.func.__globals__)
             return transform(value)
 
-
-        for rule_name in self.rules:
-            kwargs[rule_name] = eval_type(self.rules[rule_name], matches[rule_name])
+        for rule_name in self.route_rules:
+            kwargs[rule_name] = eval_type(self.route_rules[rule_name], matches[rule_name])
 
         vargs += kwargs.values()
 
@@ -85,39 +82,44 @@ class WebRoute(resource.Resource):
     def render(self, request):
         return self.run(request)
 
+    def getChild(self, child_name, request):
+        return self
 
-class Site(object):
+
+
+
+
+
+class WebSite(server.Site):
 
     def __init__(self):
         self.routes = {}
+        self.double_slash_warning = True
+
+        self.no_resource_cls = NoResource
+
+        server.Site.__init__(self, self.no_resource_cls())
+
 
     def add(self, route_str):
 
-        if isinstance(route_str, str) is False:
-            raise ValueError(f"Expecting {route_str} to be <type str>")
-
         def decorator(func):
+            action = ViewResource(route_str, func, self.double_slash_warning)
+            self.routes[action.regex] = action
 
-            processed_route = process_route(route_str, func)
-            self.routes[processed_route.regex] = processed_route
             return func
 
         return decorator
 
+    def getResourceFor(self, request):
 
-    def routeRequest(self, request):
-        for route_regex, route in self.routes.items():
-            if route_regex.match(request.path) is not None:
-                return route
-
+        for path_regex, web_resource in self.routes.items():
+            if path_regex.match(request.path) is not None:
+                return web_resource
         else:
-            return NoResource()
+            return self.no_resource_cls()
 
 
-site = Site()
-add = site.add
 
-
-if __name__ == "__main__":
-    import pytest
-    pytest.main()
+website = WebSite()
+add = website.add
