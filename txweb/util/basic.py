f605ed4e130aa7b721ce0b7d97f4ad9b0b033ef8
devdave <devdave@ominian.net>
2019-06-10 15:06:42 -0600

    In case it is needed later, wrap the use of __qualname__ incase it needs to be changed later

--- txweb/util/basic.py
+++ txweb/util/basic.py
@@ -10,6 +10,18 @@ def expose(target):
     target.exposed = True
     return target
 
+
+def get_thing_name(thing: object) -> str:
+    """
+        Attempts to return a unique and informative name for thing
+
+        Currently relies exclusively on __qualname__ as per https://www.python.org/dev/peps/pep-3155/
+
+    """
+
+    return getattr(thing, "__qualname__") #throws exception if it doesn't have __qualname__ property
+
+
 class ActionResource(resource.Resource):
 
     __slots__ = ['func', 'parent']
