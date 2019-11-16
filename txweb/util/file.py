c6223efe925e1fb39ac778c6e030cc8ff30dc9c3
devdave <devdave@ominian.net>
2019-11-16 11:12:16 -0700

    PEP8

--- txweb/util/file.py
+++ txweb/util/file.py
@@ -5,9 +5,14 @@ import pathlib
 
 class File(txFile):
 
-    def __init__(self, path, defaultType='text/html', ignoredExts=(), registry=None, allowExt=0):
+    def __init__(self, path, default_type='text/html', ignored_exts=(), registry=None, allow_ext=0):
 
         if pathlib.Path(path).exists() is False:
             raise ValueError(f"Unable to find {path}, file does not exist")
 
-        txFile.__init__(self, path, defaultType='text/html', ignoredExts=(), registry=None, allowExt=0)
+        txFile.__init__(self,
+                        path,
+                        defaultType=default_type,
+                        ignoredExts=ignored_exts,
+                        registry=registry,
+                        allowExt=allow_ext)
