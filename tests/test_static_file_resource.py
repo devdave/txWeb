bde26ab37d180f5a46119cc29ca229c0735fae38
devdave <devdave@ominian.net>
2019-11-21 19:04:16 -0700

    FAILING Tests - ran into a problem
    twisted.web.static.File doesn't behave how the name implies.  I feel it should be called Directory(Resource) as it isn't meant to serve a single file.

---
+++ tests/test_static_file_resource.py
@@ -0,0 +1,55 @@
+
+from txweb.web_views import WebSite
+
+from twisted.web.server import NOT_DONE_YET
+from twisted.web.static import File
+from twisted.web.resource import getChildForRequest
+
+from .helper import MockRequest
+
+from pathlib import Path
+
+
+"""
+Reference tests for how I can hack around File.
+
+One complication is that File resource has isLeaf set to 0/False so it's not meant to
+be rendered directly but instead through getChild
+"""
+def test_serves_a_file():
+
+    license = Path(__file__).parent / "fixture" / "static" / "LICENSE.txt" # type: Path
+
+    resource = File(str(license))
+
+    request = MockRequest(["LICENSE.txt"], "/LICENSE.txt")
+
+    file_resource = getChildForRequest(resource, request)
+
+    response = file_resource.render(request)
+    actual = license.read_bytes()
+
+
+    assert response == NOT_DONE_YET
+    assert len(request.written) == 1
+    expected = request.written[0]
+    assert len(actual) == len(expected)
+
+
+def test_serves_a_directory():
+    license = Path(__file__).parent / "fixture" / "static" / "LICENSE.txt"  # type: Path
+
+    request = MockRequest(["irrelevant","past","path"], "license.txt")
+
+    file_resource = File(str(license.parent))
+    dir_resource = file_resource.getChild("license.txt", request)
+
+
+    response = dir_resource.render(request)
+
+    actual = license.read_bytes()
+    assert response == NOT_DONE_YET
+    assert len(request.written) == 1
+    expected = request.written[0]
+    assert len(actual) == len(expected)
+
