094a38fc99ac927d17dae733df37bd8872c7601f
devdave <devdave@ominian.net>
2019-11-25 06:48:21 -0700

    additional type hints

--- txweb/resources/directory.py
+++ txweb/resources/directory.py
@@ -4,11 +4,12 @@ from txweb.resources import SimpleFile
 from twisted.web.resource import Resource
 
 from pathlib import Path
+import typing as T
 
 
 class Directory(Resource):
 
-    def __init__(self, path, recurse=False):
+    def __init__(self, path:T.Union[str, Path], recurse:bool=False):
         """
 
         :param path: str/Path an ABSOLUTE filepath to a directory to be exposed to a http client
@@ -28,13 +29,14 @@ class Directory(Resource):
         self._render_GET = func
         return func
 
-    def allowedFiles(self):
+    def allowedFiles(self) -> T.List[str]:
         return [file.name for file in self.path.glob("*") if file.exists() and file.is_file()]
 
 
 
-    def getChild(self, path, request: StrRequest):
+    def getChild(self, path:bytes, request: StrRequest):
 
+        path = path.decode("utf-8")
         if path == "/" or path == "":
             return self
 
