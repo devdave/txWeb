afa393199c208bb8f5ee4ec2b1938cb07c9ff45f
devdave <devdave@ominian.net>
2019-11-26 07:21:35 -0700

    expose routing resource

--- txweb/resources/__init__.py
+++ txweb/resources/__init__.py
@@ -3,5 +3,6 @@ from .view_class import ViewClassResource
 from .view_function import ViewFunctionResource
 from .simple_file import SimpleFile
 from .directory import Directory
+from .routing import RoutingResource
 
-__ALL__ = ["ViewClassResource", "ViewFunctionResource", "SimpleFile", "Directory"]
+__ALL__ = ["ViewClassResource", "ViewFunctionResource", "SimpleFile", "Directory", "RoutingResource"]
