8ea5b8e95b12627f246d0547f4f0d25f0eccb485
devdave <devdave@ominian.net>
2019-12-06 21:30:14 -0700

    todo replace with better tests

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -6,22 +6,3 @@ from txweb import web_views
 from .helper import ensureBytes, MockRequest
 
 
-
-def test_override_404_resource():
-
-    class My404(tw_resource.Resource):
-        def render(self):
-            return "There was an error"
-
-
-    website = web_views.WebSite()
-
-    @website.onError
-    def handle_error(site, exc):
-        return My404()
-
-
-    request = MockRequest("/not/real")
-    response = website.getResourceFor(request)
-
-    assert isinstance(response, My404)
