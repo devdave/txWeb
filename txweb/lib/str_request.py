80139abc7201ef1712179736f26dad102fb6053d
devdave <devdave@ominian.net>
2019-12-23 06:08:22 -0700

    Explicit import for MultiDict from werkzeug

--- txweb/lib/str_request.py
+++ txweb/lib/str_request.py
@@ -24,6 +24,7 @@ from twisted.web.http import _parseHeader
 from twisted.python.compat import _PY3, _PY37PLUS, nativeString, escape, intToBytes
 
 from werkzeug.formparser import FormDataParser
+from werkzeug import MultiDict
 
 import cgi
 import json
