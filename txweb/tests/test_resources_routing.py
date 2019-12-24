d5c463191e64b1474086087cdf2702a9943c2dd8
devdave <devdave@ominian.net>
2019-12-24 14:39:12 -0700

    failing test

--- txweb/tests/test_resources_routing.py
+++ txweb/tests/test_resources_routing.py
@@ -1,5 +1,10 @@
 from txweb.resources import RoutingResource
 
+from txweb import App
+
+import typing as T
+from .helper import RequestRetval
+
 
 def test_instantiates_without_error():
 
@@ -9,3 +14,19 @@ def test_instantiates_without_error():
     fake_site = FakeSite()
 
     resource = RoutingResource(fake_site)
+
+
+def test_how_head_requests_are_handled(dummy_request:RequestRetval):
+
+    app = App(__name__)
+
+    @app.add("/foo", methods=["POST"])
+    def handle_foo(request):
+        return b"123"
+
+    dummy_request.request.site = app.site
+    dummy_request.channel.site = app.site
+    dummy_request.request.requestReceived(b"HEAD", b"/foo", b"HTTP1/1")
+
+
+
