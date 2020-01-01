0e11cebe8bd9cbbe3822ae9bccfe31b8f8894f49
devdave <devdave@ominian.net>
2020-01-01 10:02:14 -0700

    Fixed up error handling tests and added Failing test for default DebugHandler

---
+++ txweb/tests/test_errors_handlers_DebugHandler.py
@@ -0,0 +1,38 @@
+
+from txweb import Application
+from txweb.lib.errors.handler import DebugHandler
+from txweb.lib.str_request import StrRequest
+
+from .helper import MockRequest
+from .helper import RequestRetval
+
+def test_handler_doesnt_blowup():
+
+    app = Application(__name__)  # type: Application
+
+    handler = DebugHandler(app)
+    app.add_error_handler(handler, "default", override=True)
+
+
+def test_handler_catches_error(dummy_request:RequestRetval):
+
+    app = Application(__name__)
+    dummy_request.request.site = app.site
+    dummy_request.channel.site = app.site
+
+    @app.add("/foo")
+    def handle_foo(request):
+        raise Exception()
+
+    dummy_request.request.requestReceived(b"GET", b"/foo", "HTTP/1.1")
+
+    dummy_request.request.content.seek(0,0)
+    content = dummy_request.request.content.read()
+
+    assert len(content) > 0
+    assert dummy_request.request.code == 500
+    assert dummy_request.request.code_message == b"Internal server error"
+
+
+
+
