0f6ea61007c3ecd6faa4f4cba3dc585088818a1d
devdave <devdave@ominian.net>
2020-01-10 22:08:10 -0700

    Update __init__.py

--- txweb/resources/__init__.py
+++ txweb/resources/__init__.py
@@ -2,7 +2,7 @@
 from .view_class import ViewClassResource
 from .view_function import ViewFunctionResource
 from .simple_file import SimpleFile
-from .directory import Directory
 from .routing import RoutingResource
+from .directory import Directory
 
 __ALL__ = ["ViewClassResource", "ViewFunctionResource", "SimpleFile", "Directory", "RoutingResource"]
