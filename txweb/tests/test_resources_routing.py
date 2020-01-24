f89ad859627913bb74f84f8b00ce4ea5032239a8
devdave <devdave@ominian.net>
2020-01-24 10:56:49 -0700

    new tests for coverage purposes

--- txweb/tests/test_resources_routing.py
+++ txweb/tests/test_resources_routing.py
@@ -36,4 +36,53 @@ def test_how_head_requests_are_handled(dummy_request:RequestRetval):
     dummy_request.request.requestReceived(b"HEAD", b"/foo", b"HTTP1/1")
 
 
+def test_ensure_blows_up_with_a_bad_add():
+
+    app = App(__name__)
+    bad_asset = sentinel
+
+
+    with pytest.raises(ValueError) as excinfo:
+        app.add("/trash")(bad_asset)
+
+        assert "expected callable|Object|twisted.web.resource.Resource" in str(excinfo.value)
+
+
+def test_ensure_blowsup_with_a_class_that_has_no_way_to_render():
+
+    app = App(__name__)
+
+    with pytest.raises(UnrenderableException):
+        @app.add("/trash")
+        class BasClass(object):
+            pass
+
+def test_ensure_a_classic_like_class_is_routed():
+
+
+    app = App(__name__)
+
+
+    @app.add("/trash")
+    class GoodClass(object):
+
+        def render(self, request):
+            return b"Rendered"
+
+    first_key = next(iter(app.router.iter_rules()))
+    endpoint = app.router._endpoints[first_key.endpoint]
+    assert isinstance(endpoint, ViewClassResource)
+    debug = 1
+
+def test_ensure_resource_is_added():
+
+    app = App(__name__)
+
+    app.add_resource("/404", resource=NoResource())
+
+    first_key = next(iter(app.router.iter_rules()))
+    endpoint = app.router._endpoints[first_key.endpoint]
+    assert isinstance(endpoint, NoResource)
+    debug = 1
+
 
