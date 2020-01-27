b018dbfd4eb80a2753e2f53d6f1c23a99730ea94
devdave <devdave@ominian.net>
2020-01-26 18:25:35 -0700

    Bandaid - TODO fix this up

--- source/conf.py
+++ source/conf.py
@@ -63,7 +63,7 @@ html_static_path = ['_static']
 
 def setup(app):
     app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
+            'url_resolver': lambda url: "TODO" + url,
             'auto_toc_tree_section': 'Contents',
             }, True)
     app.add_transform(AutoStructify)
