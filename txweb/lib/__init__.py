2bea5e7b2cda2547e0a7a1aa1160f1307ce46b53
devdave <devdave@ominian.net>
2020-02-02 18:42:42 -0700

    add new pre/post filters

--- txweb/lib/__init__.py
+++ txweb/lib/__init__.py
@@ -1,3 +1,6 @@
 from .str_request import StrRequest
 from .view_class_assembler import expose as expose_method
 from .view_class_assembler import ViewClassResource
+from .view_class_assembler import set_prefilter, set_postfilter
+
+__ALL__ = ["StrRequest", "expose_method", "ViewClassResource", "set_prefilter", "set_postfilter"]
