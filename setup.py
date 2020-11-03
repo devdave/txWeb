23effe42157173d32faae06633e5b2ab180ad800
devdave <devdave@ominian.net>
2020-11-03 14:53:49 -0700

    preparation to publish to pypi

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 META_DATA = dict(
-    name            = "txweb",
-    version         = '0.11.2019',
-    author          = "David W.",
-    author_email    = "devdave@ominian.net",
-    url             = "https://github.com/devdave/txWeb",
-    packages        = ['txweb',"txweb.util"],
-    package_data    = {
+    name            = "txweb"
+    , version         = '0.11.2020'
+    , author          = "David W."
+    , author_email    = "devdave@ominian.net"
+    , url             = "https://github.com/devdave/txWeb"
+    , packages        = find_packages(include=["txweb", "txweb.*"])
+    , package_data    = {
         'txweb': ['LICENSE.txt']
-        },
-    license         = "MIT License",
-    keywords = "twisted web alternative routing",
-    description = "An alternative routing system for use with twisted.web",
-    requires = ["decorator", "twisted", "werkzeug"]
+        }
+    , license         = "MIT License"
+    , keywords = "twisted web alternative routing"
+    , description = "An alternative routing system for use with twisted.web"
+    , install_requires = ["decorator", "twisted", "werkzeug"]
+    , tests_require = ["pytest", "pytest-catchlog"]
+    , extras_require = {
+        "development": ["pytest", "jinja2", "pytest-catchlog", "pytest-cov", "coverage"]
+    }
 )
 
 if __name__ == '__main__':
