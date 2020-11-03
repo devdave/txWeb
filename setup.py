e04b2f88057faf9c6b8265053d37852740a21260
devdave <devdave@ominian.net>
2020-11-03 15:35:10 -0700

    Turns out jinja2 is a core requirement

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 META_DATA = dict(
-    , packages        = find_packages(include=["txweb", "txweb.*"])
-    , package_data    = {
+    packages            = find_packages(include=["txweb", "txweb.*"])
+    , package_data      = {
         'txweb': ['LICENSE.txt']
         }
-    , license         = "MIT License"
-    , keywords = "twisted web alternative routing"
-    , description = "An alternative routing system for use with twisted.web"
-    , install_requires = ["decorator", "twisted", "werkzeug"]
-    , setup_requires=["pytest-runner"]
-    , tests_require = ["pytest", "pytest-catchlog"]
-    , extras_require = {
+    , license           = "MIT License"
+    , keywords          = "twisted web alternative routing"
+    , description       = "An alternative routing system for use with twisted.web"
+    , install_requires  = ["decorator", "twisted", "werkzeug", "jinja2"]
+    , setup_requires    = ["pytest-runner"]
+    , tests_require     = ["pytest", "pytest-catchlog"]
+    , extras_require    = {
         "development": ["pytest", "jinja2", "pytest-catchlog", "pytest-cov", "coverage"]
     }
 )
