8b531c7158aac02f1ca78a669bf8c4b62fbd38f1
devdave <devdave@ominian.net>
2020-11-09 18:15:04 -0700

    Switch over to using twisted.logger instead of stdlib logging

--- txweb/log.py
+++ txweb/log.py
@@ -1,5 +1,7 @@
 
-from logging import getLogger as stdlib_getLogger, Logger
+# from logging import getLogger as stdlib_getLogger, Logger
+from twisted import logger
 
-def getLogger(namespace: str) -> Logger:
-    return stdlib_getLogger(namespace)
+
+def getLogger(namespace: str = None) -> logger.Logger:
+    return logger.Logger(namespace) if namespace else logger.Logger()
