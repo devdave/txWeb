d8b67143ea7468d5917cf1ce463e2ceac76074fa
devdave <devdave@ominian.net>
2020-11-27 16:02:21 -0700

    no coverage, they work or they don't

--- txweb/lib/message_handler.py
+++ txweb/lib/message_handler.py
@@ -5,38 +5,41 @@ from twisted.web.server import NOT_DONE_YET
 
 from collections.abc import Mapping
 
-if T.TYPE_CHECKING or False:
+if T.TYPE_CHECKING or False: # pragma: no cover
     # recursion import
     from .at_wsprotocol import AtWSProtocol
 
 
 
 
-class MessageHandler(Mapping):
+class MessageHandler(Mapping): #pragma: no cover
+
+    raw_message: T.Dict[T.str, T.Any]
+    connection: AtWSProtocol
 
     def __init__(self, raw_message:dict, connection:AtWSProtocol):
         self.raw_message = raw_message # type: dict
         self.connection = connection
 
-    def __getitem__(self, item):
+    def __getitem__(self, item):  # pragma: no cover
         return self.raw_message[item]
 
-    def __iter__(self):
+    def __iter__(self):  # pragma: no cover
         return self.raw_message.__iter__()
 
-    def __len__(self):
+    def __len__(self):  # pragma: no cover
         return len(self.raw_message)
 
-    def __contains__(self, item):
+    def __contains__(self, item): # pragma: no cover
         return item in self.raw_message
 
-    def keys(self):
+    def keys(self):  # pragma: no cover
         return self.raw_message1.keys()
 
-    def items(self):
+    def items(self):  # pragma: no cover
         return self.raw_message.items()
 
-    def values(self):
+    def values(self):  # pragma: no cover
         return self.raw_message.values()
 
     def get(self, key, default=None, type=None):
@@ -83,19 +86,13 @@ class MessageHandler(Mapping):
 
         return value
 
-    @property
-    def is_asking(self):
-        return "caller_id" in self.raw_message
-
-    def respond(self, **kwargs):
-        self.connection.respondAsDict(self.raw_message, result=kwargs)
-        return NOT_DONE_YET
+    def respond(self, result):
+        return self.connection.respond(self.raw_message, result=result)
 
     def tell(self, endpoint, **kwargs):
-        self.connection.sendDict(endpoint=endpoint, type="tell", args=kwargs)
+        return self.connection.tell(endpoint, **kwargs)
 
     def ask(self, endpoint, **kwargs):
-        #TODO setup a deferred somewhere in here or below in protocol
         return self.connection.ask(endpoint, type="ask", args=kwargs)
 
     def get_session(self, get_key=None):
