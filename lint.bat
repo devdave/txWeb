0392b9cb1c7b56e28c806b0b66d88868a2cf22c9
devdave <devdave@ominian.net>
2020-12-04 16:50:22 -0700

    Getting to many false positives with R1705 unnecessary else/elif after return

--- lint.bat
+++ lint.bat
@@ -1,1 +1,1 @@
-pylint --ignore-patterns=test_* --ignore=helper.py,conftest.py -d C0103 -d C0301 -d C0305 -d C0115 -d W0603 -d C0303 txweb
+pylint --ignore-patterns=test_* --ignore=helper.py,conftest.py -d R1705 -d C0103 -d C0301 -d C0305 -d C0115 -d W0603 -d C0303 txweb
