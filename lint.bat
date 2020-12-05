ed78d09d4cdf9c3bfe61ef22d89dcb44b197d697
devdave <devdave@ominian.net>
2020-12-04 20:39:45 -0700

    R0902: Too many instance attributes
    Nothing I can do or want to do about this as Application is an intentional god module.

--- lint.bat
+++ lint.bat
@@ -1,1 +1,1 @@
-pylint --ignore-patterns=test_* --ignore=helper.py,conftest.py -d R0903 -d R0901 -d R1705 -d C0103 -d C0301 -d C0305 -d C0115 -d W0603 -d C0303 txweb
+pylint --ignore-patterns=test_* --ignore=helper.py,conftest.py -d R0903 -d R0901 -d R0902 -d R1705 -d C0103 -d C0301 -d C0305 -d C0115 -d W0603 -d C0303 txweb
